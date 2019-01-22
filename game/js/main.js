d8ca1f17fa44530d8e050976bd9a35cc6ee5732f
Wilhellm Nielandt <w.nielandt@web.de>
2019-01-22 14:50:32 +0100

    Spinnen, unsichtbare Wände

--- game/js/main.js
+++ game/js/main.js
@@ -2,9 +2,15 @@ PlayState = {};
 
 
 PlayState._spawnCharacters = function (data) {
+
     // spawn hero
     this.hero = new Hero(this.game, data.hero.x, data.hero.y);
     this.game.add.existing(this.hero);
+// spawn spiders
+    data.spiders.forEach(function (spider) {
+        let sprite = new Spider(this.game, spider.x, spider.y);
+        this.spiders.add(sprite);
+    }, this);
 };
 
 function Hero(game, x, y) {
@@ -40,6 +46,10 @@ PlayState.preload = function () {
     this.game.load.audio('sfx:coin', 'audio/coin.wav');
     //Coin spritesheet
     this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
+    //Spinnen spritesheet
+    this.game.load.spritesheet('spider', 'images/spider.png', 42, 32);
+    //unsichtbare Wände
+    this.game.load.image('invisible-wall', 'images/invisible_wall.png');
 };
 
 // create game entities and set up world here
@@ -60,6 +70,9 @@ PlayState._loadLevel = function (data) {
     // Gruppen
     this.platforms = this.game.add.group();
     this.coins = this.game.add.group();
+    this.spiders = this.game.add.group();
+    this.enemyWalls = this.game.add.group();
+    this.enemyWalls.visible = false;
     //  spawnen
     this._spawnCharacters({hero: data.hero, spiders: data.spiders});
 	//alle Plattformen spawnen
@@ -69,7 +82,6 @@ PlayState._loadLevel = function (data) {
     //Schwerkraft
     const GRAVITY = 1200;
     this.game.physics.arcade.gravity.y = GRAVITY;
-
 };
 
 window.onload = function () {
@@ -85,6 +97,8 @@ PlayState._spawnPlatform = function (platform) {
     this.game.physics.enable(sprite);
     sprite.body.allowGravity = false;
     sprite.body.immovable = true;
+    this._spawnEnemyWall(platform.x, platform.y, 'left');
+    this._spawnEnemyWall(platform.x + sprite.width, platform.y, 'right');
 };
 
 PlayState.init = function () {
@@ -131,6 +145,12 @@ PlayState._handleInput = function () {
     }
 };
 PlayState._handleCollisions = function () {
+    //
+    this.game.physics.arcade.collide(this.spiders, this.platforms);
+    this.game.physics.arcade.collide(this.spiders, this.enemyWalls);
+    //
+    this.game.physics.arcade.collide(this.spiders, this.platforms);
+    //
     this.game.physics.arcade.collide(this.hero, this.platforms);
     //
     this.game.physics.arcade.overlap(this.hero, this.coins, this._onHeroVsCoin,
@@ -160,3 +180,42 @@ PlayState._onHeroVsCoin = function (her, coin) {
     this.sfx.coin.play();
     coin.kill();
 }
+function Spider(game, x, y) {
+    Phaser.Sprite.call(this, game, x, y, 'spider');
+    // anchor
+    this.anchor.set(0.5);
+    // animation
+    this.animations.add('crawl', [0, 1, 2], 8, true);
+    this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
+    this.animations.play('crawl');
+
+    // physic properties
+    this.game.physics.enable(this);
+    this.body.collideWorldBounds = true;
+    this.body.velocity.x = Spider.SPEED;
+}
+Spider.SPEED = 100;
+//
+Spider.prototype = Object.create(Phaser.Sprite.prototype);
+Spider.prototype.constructor = Spider;
+
+Spider.prototype.update = function () {
+    //wenn sie an der rechten Wand ist, dreht sie sich nach links
+    if (this.body.touching.right || this.body.blocked.right){
+        this.body.velocity.x = -Spider.SPEED;
+    }
+    //wenn sie an der linken Wand ist, dreht sie sich nach rechts
+    else if(this.body.touching.left || this.body.blocked.left){
+        this.body.velocity.x = Spider.SPEED;
+    }
+};
+
+PlayState._spawnEnemyWall = function (x, y, side) {
+    let sprite = this.enemyWalls.create(x, y, 'invisible-wall');
+    //
+    sprite.anchor.set(side === 'left' ? 1 : 0, 1);
+    //
+    this.game.physics.enable(sprite);
+    sprite.body.immovable = true;
+    sprite.body.allowGravity = false;
+};
